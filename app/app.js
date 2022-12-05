781989dccb3ef224fb3b9752587cc92d5908b553
Andrey Kuznetsov <akuznecov@usetech.ru>
2022-12-05 19:19:08 +0600

    MetaMask application example template

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 const { web3Enable, web3Accounts, web3FromAddress } = require('@polkadot/extension-dapp');
 
+const { ethers } = require('ethers');
+
+
 const sleep = async (time) => {
   await new Promise((resolve) => {
     setTimeout(() => resolve(), time);
@@ -86,6 +89,73 @@ const signTransaction = (sender, injector, transaction, label='transaction') => 
 }
 
 
+const switchMetamaskNetwork = async (rpcEndpoint) => {
+  const chainId = `0x${Number(await getChainId(rpcEndpoint)).toString(16)}`;
+  try {
+    await window.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: chainId }],
+    });
+  } catch (switchError) {
+    // This error code indicates that the chain has not been added to MetaMask.
+    if (switchError.code !== 4902) {
+      console.error(switchError);
+      return;
+    }
+    try {
+      window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [{
+          chainId: chainId,
+          rpcUrls: [rpcEndpoint],
+          chainName: 'Opal testnet',
+          nativeCurrency: {
+            name: 'OPAL',
+            symbol: 'OPL',
+            decimals: 18
+          }
+        }]
+      });
+    } catch (addError) {
+      console.error(addError);
+    }
+  }
+}
+
+const getChainId = async (rpcEndpoint) => {
+  const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
+  return (await provider.getNetwork()).chainId;
+}
+
+
+const connectMetamask = async (rpcEndpoint) => {
+  await switchMetamaskNetwork(rpcEndpoint);
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  const accounts = await provider.send('eth_requestAccounts', []);
+
+  return {provider, accounts};
+}
+
+const createCollectionFromEth = async (account, provider, collection) => {
+  let result = null;
+  const signer = provider.getSigner(account);
+  const collectionHelper = new ethers.Contract('0x6c4e9fe1ae37a41e93cee429e8e1881abdcbb54f', ['function createNFTCollection(string memory name, string memory description, string memory tokenPrefix) external payable returns (address)'], signer);
+  const logReader = new ethers.utils.Interface(['event CollectionCreated(address indexed owner, address indexed collectionId)']);
+
+  try {
+    const tx = await collectionHelper.createNFTCollection(collection.name, collection.description, collection.tokenPrefix, {value: ethers.utils.parseEther('2')});
+    const txResult = await tx.wait();
+    const log = txResult.logs.map(x => logReader.parseLog(x))[0];
+    result = log.args.collectionId;
+  } catch(e) {
+    console.error(e);
+    return;
+  }
+  return result;
+};
+
 module.exports = {
-  getAccounts, transfer, connect
+  getAccounts, transfer, connect, switchMetamaskNetwork, getChainId, connectMetamask, createCollectionFromEth
 }
